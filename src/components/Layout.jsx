60e89bb0fa58d4d990629187b71bee8a46c07af8
snpzone <65709045+snpzone@users.noreply.github.com>
2025-06-18 15:52:57 +0530

    fial commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -13,7 +13,7 @@ const Layout = () => {
   return (
     <div className="container-fluid p-0 m-0 ">
       <div className="row no-gutters" style={{ height: "100vh" }}>
-        <div className="col-3 p-3 " style={{ backgroundColor: "red" }}>
+        <div className="col-3 p-3 " style={{ backgroundColor: "rgb(165, 214, 250)" }}>
           <ProfileCard />
          
           <ToggleSwitch layout={layout} toggleLayout={toggleLayout} />
